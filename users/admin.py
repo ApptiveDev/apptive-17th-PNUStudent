b126d0eea27d976cca08ea41d799cdce08d8921d
nstgic3 <nstgic3@gmail.com>
2022-11-30 00:51:58 +0900

    FEATURE UserAdmin view

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (
+            "Profile",
+            {
+                "fields": (
+                    "username",
+                    "password",
+                    "name",
+                    "major",
+                ),
+            },
+        ),
+        (
+            "Permission",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+                "classes": ("collapse",),
+            },
+        ),
+        (
+            "Importanat Dates",
+            {
+                "fields": (
+                    "last_login",
+                    "date_joined",
+                ),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+
+    list_display = ("username", "email", "name")
