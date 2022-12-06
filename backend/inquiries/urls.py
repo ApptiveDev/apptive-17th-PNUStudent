00c794f542ea118b99cc7fe50143437781c9c9cc
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2022-12-06 21:17:25 +0900

    Delete backend

--- backend/inquiries/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path("main/", views.InquiriesList.as_view()),
-    path("<int:pk>", views.InquiryDetail.as_view()),
-    path("<int:pk>/answer", views.InquiryAnswerDetail.as_view()),
-]
--- backend/medias/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import PhotoDetail
-
-urlpatterns = [
-    path("photos/<int:pk>", PhotoDetail.as_view()),
-]
