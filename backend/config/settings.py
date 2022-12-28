aa7beec85c5fc73eeb72dab87c88a6f33e607df3
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2022-12-28 20:21:11 +0900

    SET CORS_ALLOW_HEADERS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -85,6 +85,18 @@ CORS_ALLOW_METHODS = (
     'PUT',
 )
 
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
+
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
